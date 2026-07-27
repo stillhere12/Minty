cfe6f7217318cebaafb306d298435a15c756ef97
Balaji <balajireset744@gmail.com>
2026-07-27 15:07:35 +0530

    Removed Rust part will Implement later. Cleaned the repo. Summary: Test Plan:

--- Mint/src/main.rs
+++ Mint/src/main.rs
@@ -1,24 +1,5 @@
 // This  just currently works for infix operators
 // TODO make it work for all
-
-enum Node {
-    Operand(char),
-    Operator {
-        op: char,
-        priority: u8,
-        left: Box<Node>,
-        right: Box<Node>,
-    },
+fn main() {
+    println!("Hello, World!");
 }
-impl Node {
-    // fxn to create operand leaf node.
-    fn operand(val: char) -> Self {
-        Node::Operand(val)
-    }
-    fn insert_operator(self, op, priority, right_operand:Node)->Self {
-        match self {
-
-        }
-    }
-}
-fn main() {}
